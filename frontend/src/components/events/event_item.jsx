d30e3432dba0ecc3bcf4504edd767995ee42f829
stewart morales <stewartmorales129@gmail.com>
2021-03-30 11:19:39 -0400

    Create events index

---
+++ frontend/src/components/events/event_item.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const EventItem = ({event}) => {
+  return (
+    <div className="event-single-container">
+      <img src="" alt="" className="event-thumbnail"/>
+      <div className="event-info">
+        <p>{event.dateTime}</p>
+        <Link to={`/events/${event._id}`}><h3>{event.title}</h3></Link>
+        <p>{event.description}</p>
+        <p>{event.maxCapacity} attendees</p>
+      </div>
+    </div>
+  )
+}
+
+export default EventItem;
