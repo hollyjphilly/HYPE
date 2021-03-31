033ab1c0c63879399aefd75a1aafe981c16aac64
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-03-31 11:29:10 -0400

    Edit stylesheet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import jwt_decode from 'jwt-decode';
 import { setAuthToken } from './util/session_api_util';
 import { logout } from './actions/session_actions';
 import axios from 'axios';
-import './index.scss';
+import './assets/stylesheets/index.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
