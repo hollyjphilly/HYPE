244ffc5ddecccdd991b23c46dedc9095c9c83e48
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-03-31 10:26:27 -0400

    Fix compile warnings, give imgs alt text, make logo clickable

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -22,26 +22,26 @@ class Main extends React.Component {
           <div className="splash-body-content">
             
           <div className="section" id="one">
-            <img src="https://i.ibb.co/N3c3qSf/Screen-Shot-2021-03-31-at-3-24-20-AM.png" />
+            <img src="https://i.ibb.co/N3c3qSf/Screen-Shot-2021-03-31-at-3-24-20-AM.png" alt="Play dodgeball, capture the flag, or freeze tag"/>
             <div className="section-description">
               <h1>Recess lives on.</h1>
               School yard games don’t have to end just because you’re an adult. 
             </div>
           </div>
           <div className="section" id="scroller">
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
-              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" />
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
+              <img src="https://i.ibb.co/SrDc9mj/Screen-Shot-2021-03-31-at-5.png" alt="GET HYPED -"/>
           </div>
 
           <div className="section" id="two">
-            <img src="https://i.ibb.co/bRPXJ64/Screen-Shot-2021-03-31-at-4-13-08-AM.png" />
+            <img src="https://i.ibb.co/bRPXJ64/Screen-Shot-2021-03-31-at-4-13-08-AM.png" alt="Play classics like basketball, soccer, football"/>
             <div className="section-description">
               <h1>Play the classics.</h1>
               Find games of sports you know and love. 
@@ -50,7 +50,7 @@ class Main extends React.Component {
 
           <div className="section" id="three">
             
-              <h1>Or try something new!</h1>
+              <h1>Or try something new! </h1>
               <a href="/#/events" className="splash-button">find a game to get hyped about</a> 
             
           </div>
