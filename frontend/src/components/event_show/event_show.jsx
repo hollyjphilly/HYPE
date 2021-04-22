4dd95b20e073ae7e5c9b9035d02d355b9d6310a4
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-04-22 19:28:59 -0400

    fix comments

--- frontend/src/components/event_show/event_show.jsx
+++ frontend/src/components/event_show/event_show.jsx
@@ -208,47 +208,6 @@ class EventShow extends React.Component {
           </div>
         </div>
     </div>
-      // <div className="event-show-main-div">
-      //   <div className="event-container">
-      //     <div className="single-event-container">
-      //       {/* <div className="event-images">IMAGES GO HERE</div> */}
-      //       <div className="event-info-container">
-      //         <div className="event-header">
-      //           <div className="event-header-text">
-      //             <h2>{showEvent.title}</h2>
-      //             <p>{`${date} ${time}`}</p>
-      //             <p>{showEvent.address}</p>
-      //           </div>
-      //           {joinButton()}
-      //         </div>
-      //         <div className="single-event-description">
-      //           <h3 className="single-event-description-h3">
-      //             Hosted by {showEvent.host.username}
-      //           </h3>
-      //           <h3 className="single-event-description-h3">
-      //             There are{" "}
-      //             {showEvent.maxCapacity - showEvent.usersAttending.length}{" "}
-      //             spots left.
-      //           </h3>
-      //           <p className="single-event-details-p">
-      //             {showEvent.description}
-      //           </p>
-      //         </div>
-      //       </div>
-      //     </div>
-
-      //     <div className="event-map">
-      //       <WrappedMap
-      //         googleMapURL={
-      //           "https://maps.googleapis.com/maps/api/js?key=AIzaSyDVt-WmXfXrG4hDwxbM6Ctir_Q8e1VicE8"
-      //         }
-      //         loadingElement={<div style={{ height: "100%" }} />}
-      //         containerElement={<div style={{ height: "100%" }} />}
-      //         mapElement={<div style={{ height: "100%" }} />}
-      //       />
-      //     </div>
-      //   </div>
-      // </div>
     );
   }
 }
